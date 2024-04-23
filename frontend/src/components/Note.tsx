84a65067c33ea4f848d02b722b8bc1b08cb6a48b
SUCHV\suchv <admix228@gmail.com>
2024-04-23 23:37:02 +0200

    Update and delete notes functionalities added. Icons for button and notes added. Css cleaned up.

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,14 +1,18 @@
 import styles from "../styles/Note.module.css";
+import styleUtils from "../styles/utils.module.css";
 import { Card } from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
 import { formatDate } from "../utils/formatDate";
+import { MdDelete } from "react-icons/md";
 
 interface NoteProps {
     note: NoteModel,
+    onNoteClicked: (note: NoteModel) => void,
+    onDeleteNoteClicked: (note: NoteModel) => void,
     className?: string,
 }
 
-const Note = ({ note, className }: NoteProps) => {
+const Note = ({ note, onNoteClicked, onDeleteNoteClicked, className }: NoteProps) => {
     const {
         title,
         text,
@@ -20,14 +24,23 @@ const Note = ({ note, className }: NoteProps) => {
     if(updatedAt > createdAt) {
         createdUpdatedText = "Updated: " + formatDate(updatedAt);
     } else {
-        createdUpdatedText = "Created: " +formatDate(createdAt);
+        createdUpdatedText = "Created: " + formatDate(createdAt);
     }
 
     return (
-        <Card className={`${styles.noteCard} ${className}`}>
+        <Card 
+        className={`${styles.noteCard} ${className}`}
+        onClick={() => onNoteClicked(note)}>
             <Card.Body className={styles.cardBody}>
-                <Card.Title>
-                    {note.title}
+                <Card.Title className={styleUtils.flexCenter}>
+                    {title}
+                    <MdDelete
+                    className="text-muted ms-auto"
+                    onClick={(e) => {
+                        onDeleteNoteClicked(note);
+                        e.stopPropagation();
+                    }}
+                    />
                 </Card.Title>
                 <Card.Text className={styles.cardText}>
                     {text}
